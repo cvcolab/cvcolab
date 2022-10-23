6f3c5a852bfef0182d4b0ab753ab3111bc2d5aeb
Admin <>
2022-10-23 23:00:25 +0530

    Template gallery page Template selection box

--- js/index1script.js
+++ js/index1script.js
@@ -5,3 +5,20 @@ $(document).ready(function(){
     centerMode: true,
   });
 });
+
+$(document).ready(function(){
+  $('#sampleResumeCarousel').slick({
+    slidesToShow: 1,
+    dots:true,
+    centerMode: true,
+  });
+});
+
+$(document).ready(function() {
+  $('#productListCarousel').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots:true,
+  });
+})
